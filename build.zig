429755734e1e1dd841e048f7c03b3366540bec67
Luna <git@l4.pm>
2022-04-03 00:11:21 -0300

    remove 'run' build step
    as this is an assortment of tools, run only makes sense if we create
    some 6 other run commands...

--- build.zig
+++ build.zig
@@ -12,22 +12,6 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("awtfdb-manage", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
-
-    deps.addAllTo(exe);
-
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
     const exe_tests = b.addTest("src/main.zig");
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
@@ -35,6 +19,12 @@ pub fn build(b: *std.build.Builder) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
+
+    const manage_exe = b.addExecutable("awtfdb-manage", "src/main.zig");
+    manage_exe.setTarget(target);
+    manage_exe.setBuildMode(mode);
+    manage_exe.install();
+    deps.addAllTo(manage_exe);
 
     const watcher_exe = b.addExecutable("awtfdb-watcher", "src/rename_watcher_main.zig");
     watcher_exe.setTarget(target);
