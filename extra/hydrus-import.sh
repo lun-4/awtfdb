f4a07dd4b2b20b2e30c3a261d8e209221a455aec
Luna <git@l4.pm>
2022-04-06 19:32:16 -0300

    add hydrus import script

---
+++ extra/hydrus-import.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# hydrus-import.sh - import hydrus exports into awtfdb
+#
+# hydrus has an export button! it's kinda cool, if you want to take your
+# entire library out, select 'system:everything', right click until you
+# find the export menu, select your wanted filename tag, and boom.
+#
+# implemented in shell because that's easier instead of writing a full
+# tag inferrer at the moment. maybe making one would be helpful in the long term.
+
+set -eux
+
+export_folder=$1
+
+find "$export_folder" -type f |
+while IFS= read -r filename; do
+    echo "processing $filename";
+    set +e
+    tag_file_contents=$(cat "$filename.txt")
+    if [ $? != "0" ]; then
+        echo "$filename does not have matching tag file, ignoring"
+        continue
+    fi
+    set -e
+    ainclude_tags=$(echo "$tag_file_contents" | awk '{ printf "--tag \"%s\" ", $0; }')
+    echo "$ainclude_tags '$filename'" | xargs ainclude
+done
