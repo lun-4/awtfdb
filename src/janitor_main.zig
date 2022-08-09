e8166667dff41736219c05952dc0069b32d4636f
Luna <git@l4.pm>
2022-08-09 20:26:52 -0300

    refactor janitor a bit

--- src/janitor_main.zig
+++ src/janitor_main.zig
@@ -5,6 +5,7 @@ const libpcre = @import("libpcre");
 const Context = manage_main.Context;
 
 const log = std.log.scoped(.awtfdb_janitor);
+pub const io_mode = .evented;
 
 const VERSION = "0.0.1";
 const HELPTEXT =
@@ -23,85 +24,148 @@ const HELPTEXT =
     \\ 					the index file, only run this manually)
 ;
 
-pub fn main() anyerror!u8 {
-    const rc = sqlite.c.sqlite3_config(sqlite.c.SQLITE_CONFIG_LOG, manage_main.sqliteLog, @as(?*anyopaque, null));
-    if (rc != sqlite.c.SQLITE_OK) {
-        std.log.err("failed to configure: {d} '{s}'", .{
-            rc, sqlite.c.sqlite3_errstr(rc),
-        });
-        return error.ConfigFail;
-    }
+const Counter = struct { total: usize = 0, unrepairable: usize = 0 };
+const ErrorCounters = struct {
+    file_not_found: Counter = .{},
+    incorrect_hash_files: Counter = .{},
+    incorrect_hash_cores: Counter = .{},
+    unused_hash: Counter = .{},
+};
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    var allocator = gpa.allocator();
+const StringList = std.ArrayList([]const u8);
+const Args = struct {
+    help: bool = false,
+    version: bool = false,
+    repair: bool = false,
+    full: bool = false,
+    only: StringList,
+};
 
-    var args_it = std.process.args();
-    _ = args_it.skip();
+pub fn janitorCheckCores(
+    ctx: *Context,
+    counters: *ErrorCounters,
+    given_args: Args,
+) !void {
+    var cores_stmt = try ctx.db.?.prepare(
+        \\ select core_hash, core_data
+        \\ from tag_cores
+        \\ order by core_hash asc
+    );
+    defer cores_stmt.deinit();
+    var cores_iter = try cores_stmt.iterator(struct {
+        core_hash: i64,
+        core_data: sqlite.Blob,
+    }, .{});
+    while (try cores_iter.nextAlloc(ctx.allocator, .{})) |core_with_blob| {
+        defer ctx.allocator.free(core_with_blob.core_data.data);
 
-    const StringList = std.ArrayList([]const u8);
-    const Args = struct {
-        help: bool = false,
-        version: bool = false,
-        repair: bool = false,
-        full: bool = false,
-        only: StringList,
-    };
+        const calculated_hash = try ctx.calculateHashFromMemory(
+            core_with_blob.core_data.data,
+            .{ .insert_new_hash = false },
+        );
 
-    var given_args = Args{ .only = StringList.init(allocator) };
-    defer {
-        for (given_args.only.items) |path| allocator.free(path);
-        given_args.only.deinit();
-    }
+        var hash_with_blob = (try ctx.db.?.oneAlloc(
+            Context.HashWithBlob,
+            ctx.allocator,
+            \\ select id, hash_data
+            \\ from hashes
+            \\ where id = ?
+        ,
+            .{},
+            .{core_with_blob.core_hash},
+        )).?;
+        defer ctx.allocator.free(hash_with_blob.hash_data.data);
 
-    var state: enum { None, Only } = .None;
+        const upstream_hash = hash_with_blob.toRealHash();
 
-    while (args_it.next()) |arg| {
-        switch (state) {
-            .Only => {
-                try given_args.only.append(try std.fs.path.resolve(
-                    allocator,
-                    &[_][]const u8{arg},
-                ));
-                state = .None;
-                continue;
-            },
-            .None => {},
+        if (!std.mem.eql(u8, &calculated_hash.hash_data, &upstream_hash.hash_data)) {
+            counters.incorrect_hash_cores.total += 1;
+            counters.incorrect_hash_cores.unrepairable += 1;
+
+            log.err(
+                "hashes are incorrect for tag core {d} ({s} != {s})",
+                .{ core_with_blob.core_hash, calculated_hash, upstream_hash },
+            );
+
+            if (given_args.repair) {
+                return error.ManualInterventionRequired;
+            }
+
+            continue;
         }
-        if (std.mem.eql(u8, arg, "-h")) {
-            given_args.help = true;
-        } else if (std.mem.eql(u8, arg, "-V")) {
-            given_args.version = true;
-        } else if (std.mem.eql(u8, arg, "--repair")) {
-            given_args.repair = true;
-        } else if (std.mem.eql(u8, arg, "--full")) {
-            given_args.full = true;
-        } else if (std.mem.eql(u8, arg, "--only")) {
-            state = .Only;
-        } else {
-            return error.InvalidArgument;
+
+        // TODO validate if there's any tag names to the core
+    }
+}
+
+pub fn janitorCheckUnusedHashes(
+    ctx: *Context,
+    counters: *ErrorCounters,
+    given_args: Args,
+) !void {
+    var hashes_stmt = try ctx.db.?.prepare(
+        \\ select id
+        \\ from hashes
+        \\ order by id asc
+    );
+    defer hashes_stmt.deinit();
+    var hashes_iter = try hashes_stmt.iterator(i64, .{});
+    while (try hashes_iter.next(.{})) |hash_id| {
+        const core_count = (try ctx.db.?.one(
+            i64,
+            \\ select count(*) from tag_cores
+            \\ where core_hash = ?
+        ,
+            .{},
+            .{hash_id},
+        )).?;
+
+        if (core_count > 0) continue;
+
+        const file_count = (try ctx.db.?.one(
+            i64,
+            \\ select count(*) from files
+            \\ where file_hash = ?
+        ,
+            .{},
+            .{hash_id},
+        )).?;
+
+        if (file_count > 0) continue;
+
+        const pool_count = (try ctx.db.?.one(
+            i64,
+            \\ select count(*) from pools
+            \\ where pool_hash = ?
+        ,
+            .{},
+            .{hash_id},
+        )).?;
+
+        if (pool_count > 0) continue;
+
+        log.warn("unused hash in table: {d}", .{hash_id});
+
+        counters.unused_hash.total += 1;
+
+        if (given_args.repair) {
+            try ctx.db.?.exec(
+                \\ delete from hashes
+                \\ where id = ?
+            ,
+                .{},
+                .{hash_id},
+            );
+            log.info("deleted hash {d}", .{hash_id});
         }
     }
+}
 
-    if (given_args.help) {
-        std.debug.print(HELPTEXT, .{});
-        return 1;
-    } else if (given_args.version) {
-        std.debug.print("awtfdb-janitor {s}\n", .{VERSION});
-        return 1;
-    }
-
-    var ctx = Context{
-        .home_path = null,
-        .args_it = undefined,
-        .stdout = undefined,
-        .db = null,
-        .allocator = allocator,
-    };
-    defer ctx.deinit();
-
-    try ctx.loadDatabase(.{});
-
+pub fn janitorCheckFiles(
+    ctx: *Context,
+    counters: *ErrorCounters,
+    given_args: Args,
+) !void {
     var stmt = try ctx.db.?.prepare(
         \\ select file_hash, local_path
         \\ from files
@@ -118,21 +182,8 @@ pub fn main() anyerror!u8 {
         .{},
     );
 
-    const Counter = struct { total: usize = 0, unrepairable: usize = 0 };
-
-    var counters: struct {
-        file_not_found: Counter = .{},
-        incorrect_hash_files: Counter = .{},
-        incorrect_hash_cores: Counter = .{},
-        unused_hash: Counter = .{},
-    } = .{};
-
-    var savepoint = try ctx.db.?.savepoint("janitor");
-    errdefer savepoint.rollback();
-    defer savepoint.commit();
-
-    while (try iter.nextAlloc(allocator, .{})) |row| {
-        defer allocator.free(row.local_path);
+    while (try iter.nextAlloc(ctx.allocator, .{})) |row| {
+        defer ctx.allocator.free(row.local_path);
 
         const indexed_file = (try ctx.fetchFileExact(row.file_hash, row.local_path)) orelse return error.InconsistentIndex;
         defer indexed_file.deinit();
@@ -227,119 +278,92 @@ pub fn main() anyerror!u8 {
         }
         //log.info("path {s} ok", .{row.local_path});
     }
+}
+
+pub fn main() anyerror!u8 {
+    const rc = sqlite.c.sqlite3_config(sqlite.c.SQLITE_CONFIG_LOG, manage_main.sqliteLog, @as(?*anyopaque, null));
+    if (rc != sqlite.c.SQLITE_OK) {
+        std.log.err("failed to configure: {d} '{s}'", .{
+            rc, sqlite.c.sqlite3_errstr(rc),
+        });
+        return error.ConfigFail;
+    }
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var allocator = gpa.allocator();
+
+    var args_it = std.process.args();
+    _ = args_it.skip();
+
+    var given_args = Args{ .only = StringList.init(allocator) };
+    defer {
+        for (given_args.only.items) |path| allocator.free(path);
+        given_args.only.deinit();
+    }
+
+    var state: enum { None, Only } = .None;
+
+    while (args_it.next()) |arg| {
+        switch (state) {
+            .Only => {
+                try given_args.only.append(try std.fs.path.resolve(
+                    allocator,
+                    &[_][]const u8{arg},
+                ));
+                state = .None;
+                continue;
+            },
+            .None => {},
+        }
+        if (std.mem.eql(u8, arg, "-h")) {
+            given_args.help = true;
+        } else if (std.mem.eql(u8, arg, "-V")) {
+            given_args.version = true;
+        } else if (std.mem.eql(u8, arg, "--repair")) {
+            given_args.repair = true;
+        } else if (std.mem.eql(u8, arg, "--full")) {
+            given_args.full = true;
+        } else if (std.mem.eql(u8, arg, "--only")) {
+            state = .Only;
+        } else {
+            return error.InvalidArgument;
+        }
+    }
+
+    if (given_args.help) {
+        std.debug.print(HELPTEXT, .{});
+        return 1;
+    } else if (given_args.version) {
+        std.debug.print("awtfdb-janitor {s}\n", .{VERSION});
+        return 1;
+    }
+
+    var ctx = Context{
+        .home_path = null,
+        .args_it = undefined,
+        .stdout = undefined,
+        .db = null,
+        .allocator = allocator,
+    };
+    defer ctx.deinit();
+
+    try ctx.loadDatabase(.{});
+
+    var counters: ErrorCounters = .{};
+
+    var savepoint = try ctx.db.?.savepoint("janitor");
+    errdefer savepoint.rollback();
+    defer savepoint.commit();
 
     // calculate hashes for tag_cores
-    var cores_stmt = try ctx.db.?.prepare(
-        \\ select core_hash, core_data
-        \\ from tag_cores
-        \\ order by core_hash asc
-    );
-    defer cores_stmt.deinit();
-    var cores_iter = try cores_stmt.iterator(struct {
-        core_hash: i64,
-        core_data: sqlite.Blob,
-    }, .{});
-    while (try cores_iter.nextAlloc(allocator, .{})) |core_with_blob| {
-        defer allocator.free(core_with_blob.core_data.data);
-
-        const calculated_hash = try ctx.calculateHashFromMemory(
-            core_with_blob.core_data.data,
-            .{ .insert_new_hash = false },
-        );
-
-        var hash_with_blob = (try ctx.db.?.oneAlloc(
-            Context.HashWithBlob,
-            allocator,
-            \\ select id, hash_data
-            \\ from hashes
-            \\ where id = ?
-        ,
-            .{},
-            .{core_with_blob.core_hash},
-        )).?;
-        defer allocator.free(hash_with_blob.hash_data.data);
-
-        const upstream_hash = hash_with_blob.toRealHash();
-
-        if (!std.mem.eql(u8, &calculated_hash.hash_data, &upstream_hash.hash_data)) {
-            counters.incorrect_hash_cores.total += 1;
-            counters.incorrect_hash_cores.unrepairable += 1;
-
-            log.err(
-                "hashes are incorrect for tag core {d} ({s} != {s})",
-                .{ core_with_blob.core_hash, calculated_hash, upstream_hash },
-            );
-
-            if (given_args.repair) {
-                return error.ManualInterventionRequired;
-            }
-
-            continue;
-        }
-
-        // TODO validate if there's any tag names to the core
-    }
+    //try janitorCheckFiles(&ctx, &counters, given_args);
+    //try janitorCheckCores(&ctx, &counters, given_args);
+    //try janitorCheckUnusedHashes(&ctx, &counters, given_args);
 
     // garbage collect unused entires in hashes table
 
-    var hashes_stmt = try ctx.db.?.prepare(
-        \\ select id
-        \\ from hashes
-        \\ order by id asc
-    );
-    defer hashes_stmt.deinit();
-    var hashes_iter = try hashes_stmt.iterator(i64, .{});
-    while (try hashes_iter.nextAlloc(allocator, .{})) |hash_id| {
-        const core_count = (try ctx.db.?.one(
-            i64,
-            \\ select count(*) from tag_cores
-            \\ where core_hash = ?
-        ,
-            .{},
-            .{hash_id},
-        )).?;
-
-        if (core_count > 0) continue;
-
-        const file_count = (try ctx.db.?.one(
-            i64,
-            \\ select count(*) from files
-            \\ where file_hash = ?
-        ,
-            .{},
-            .{hash_id},
-        )).?;
-
-        if (file_count > 0) continue;
-
-        const pool_count = (try ctx.db.?.one(
-            i64,
-            \\ select count(*) from pools
-            \\ where pool_hash = ?
-        ,
-            .{},
-            .{hash_id},
-        )).?;
-
-        if (pool_count > 0) continue;
-
-        log.warn("unused hash in table: {d}", .{hash_id});
-
-        counters.unused_hash.total += 1;
-
-        if (given_args.repair) {
-            try ctx.db.?.exec(
-                \\ delete from hashes
-                \\ where id = ?
-            ,
-                .{},
-                .{hash_id},
-            );
-            log.info("deleted hash {d}", .{hash_id});
-        }
-    }
-
-    const CountersTypeInfo = @typeInfo(@TypeOf(counters));
+    const CountersTypeInfo = @typeInfo(ErrorCounters);
 
     var total_problems: usize = 0;
     inline for (CountersTypeInfo.Struct.fields) |field| {
