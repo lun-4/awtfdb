7e863ab417d8dac0555519f35987a986e6ff6456
Luna <git@l4.pm>
2022-09-02 23:00:50 -0300

    fix tests

--- build.zig
+++ build.zig
@@ -15,6 +15,7 @@ pub fn build(b: *std.build.Builder) void {
     const exe_tests = b.addTest("src/main.zig");
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
+    exe_tests.use_stage1 = true;
     deps.addAllTo(exe_tests);
 
     const test_step = b.step("test", "Run unit tests");
