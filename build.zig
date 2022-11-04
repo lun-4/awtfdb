dfe46345694bd5757f1af8cbb886c116edbef871
Luna <git@l4.pm>
2022-11-04 00:08:01 -0300

    build: create single exe and copy it over to output
    close #43
    
    i like fast builds

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const deps = @import("./deps.zig");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Builder) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -20,64 +20,51 @@ pub fn build(b: *std.build.Builder) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
 
-    const manage_exe = b.addExecutable("awtfdb-manage", "src/main.zig");
-    manage_exe.setTarget(target);
-    manage_exe.setBuildMode(mode);
-    manage_exe.install();
-    deps.addAllTo(manage_exe);
+    var single_exe = b.addExecutable("wrapper-awtfdb", "src/wrapmain.zig");
+    single_exe.setTarget(target);
+    single_exe.setBuildMode(mode);
+    deps.addAllTo(single_exe);
 
-    const watcher_exe = b.addExecutable("awtfdb-watcher", "src/rename_watcher_main.zig");
-    watcher_exe.setTarget(target);
-    watcher_exe.setBuildMode(mode);
-    watcher_exe.install();
+    const hardlink_install = try b.allocator.create(CustomHardLinkStep);
+    hardlink_install.* = .{
+        .builder = b,
+        .step = std.build.Step.init(
+            .custom,
+            "link the utils",
+            b.allocator,
+            CustomHardLinkStep.make,
+        ),
+        .exe = single_exe,
+    };
+    hardlink_install.step.dependOn(&single_exe.step);
 
-    deps.addAllTo(watcher_exe);
-
-    const include_exe = b.addExecutable("ainclude", "src/include_main.zig");
-    include_exe.setTarget(target);
-    include_exe.setBuildMode(mode);
-    include_exe.install();
-
-    deps.addAllTo(include_exe);
-
-    const find_exe = b.addExecutable("afind", "src/find_main.zig");
-    find_exe.setTarget(target);
-    find_exe.setBuildMode(mode);
-    find_exe.install();
-
-    deps.addAllTo(find_exe);
-
-    const ls_exe = b.addExecutable("als", "src/ls_main.zig");
-    ls_exe.setTarget(target);
-    ls_exe.setBuildMode(mode);
-    ls_exe.install();
-
-    deps.addAllTo(ls_exe);
-
-    const tags_exe = b.addExecutable("atags", "src/tags_main.zig");
-    tags_exe.setTarget(target);
-    tags_exe.setBuildMode(mode);
-    tags_exe.install();
-    deps.addAllTo(tags_exe);
-
-    const rm_exe = b.addExecutable("arm", "src/rm_main.zig");
-    rm_exe.setTarget(target);
-    rm_exe.setBuildMode(mode);
-    rm_exe.install();
-    deps.addAllTo(rm_exe);
-
-    const janitor_exe = b.addExecutable("awtfdb-janitor", "src/janitor_main.zig");
-    janitor_exe.setTarget(target);
-    janitor_exe.setBuildMode(mode);
-    janitor_exe.install();
-    deps.addAllTo(janitor_exe);
-
-    const metrics_exe = b.addExecutable("awtfdb-metrics", "src/metrics_main.zig");
-    metrics_exe.setTarget(target);
-    metrics_exe.setBuildMode(mode);
-    metrics_exe.install();
-
-    // this is required for metrics so that the queries don't take too long
-    //metrics_exe.build_mode = .ReleaseSafe;
-    deps.addAllTo(metrics_exe);
+    b.getInstallStep().dependOn(&hardlink_install.step);
 }
+
+const CustomHardLinkStep = struct {
+    builder: *std.build.Builder,
+    step: std.build.Step,
+    exe: *std.build.LibExeObjStep,
+
+    const Self = @This();
+
+    fn make(step: *std.build.Step) !void {
+        const self: *Self = @fieldParentPtr(Self, "step", step);
+        const builder = self.builder;
+        const EXECS = .{
+            "awtfdb-manage",
+            "ainclude",
+            "awtfdb-watcher",
+            "afind",
+            "als",
+            "arm",
+            "atags",
+            "awtfdb-metrics",
+        };
+
+        inline for (EXECS) |exec| {
+            const full_dest_path = builder.getInstallPath(.{ .bin = {} }, exec);
+            try builder.updateFile(self.exe.output_path_source.path.?, full_dest_path);
+        }
+    }
+};
---
+++ src/wrapmain.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+const EXECUTABLES = .{
+    .{ "awtfdb-manage", @import("main.zig").main },
+    .{ "ainclude", @import("include_main.zig").main },
+    .{ "awtfdb-watcher", @import("rename_watcher_main.zig").main },
+    .{ "afind", @import("find_main.zig").main },
+    .{ "als", @import("ls_main.zig").main },
+    .{ "arm", @import("rm_main.zig").main },
+    .{ "atags", @import("tags_main.zig").main },
+    .{ "awtfdb-metrics", @import("metrics_main.zig").main },
+};
+
+pub fn main() anyerror!u8 {
+    var it = std.process.args();
+    const exec_name = std.fs.path.basename(it.next().?);
+    inline for (EXECUTABLES) |executable| {
+        if (std.mem.eql(u8, exec_name, executable.@"0")) {
+            const main_type = @typeInfo(@TypeOf(executable.@"1"));
+            const ret_type = @typeInfo(main_type.Fn.return_type.?);
+            const payload_type = ret_type.ErrorUnion.payload;
+            if (payload_type == u8) {
+                return executable.@"1"();
+            } else {
+                try executable.@"1"();
+                return 0;
+            }
+        }
+    }
+
+    @panic("invalid executable name");
+}
