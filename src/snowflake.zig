31f6735f27ba8289a35ae2f2460c34f057586a05
Luna <git@l4.pm>
2022-11-26 19:46:25 -0300

    fuck i64s lets use u63s it cant go wrong

--- src/snowflake.zig
+++ src/snowflake.zig
@@ -19,52 +19,49 @@ fn binaryMask(comptime bit_start: usize, comptime bit_end: usize) i64 {
     return mask;
 }
 
-pub const AnimeSnowflake = packed struct {
+const Fields = packed struct {
     timestamp: u50,
     random_bits: u13,
-
-    const Self = @This();
-
-    fn toInt(self: Self) i64 {
-        const asbytes = std.mem.asBytes(&self);
-        return @ptrCast(*const i64, asbytes).*;
-    }
-
-    fn fromInt(value: i64) AnimeSnowflake {
-        const asbytes = std.mem.asBytes(&value);
-        return @ptrCast(*const Self, asbytes).*;
-    }
 };
 
-test "snowflake" {
+pub const AnimeSnowflake = packed union {
+    value: u63,
+    fields: Fields,
+};
+
+comptime {
+    std.debug.assert(@sizeOf(AnimeSnowflake) == @sizeOf(u63));
+    std.debug.assert(@bitSizeOf(AnimeSnowflake) == 63);
+    std.debug.assert(@bitSizeOf(Fields) == 63);
+
     const timestamp: i64 = 1669493613473;
     const random_bits: u13 = 315;
-    const snowflake = AnimeSnowflake{
+    const snowflake = AnimeSnowflake{ .fields = .{
         .timestamp = @intCast(u50, timestamp),
         .random_bits = random_bits,
-    };
+    } };
 
-    const snowflake_int = snowflake.toInt();
+    const snowflake_int = snowflake.value;
+    const data2 = (AnimeSnowflake{ .value = snowflake_int }).fields;
+    std.debug.assert(timestamp == data2.timestamp);
+    std.debug.assert(random_bits == data2.random_bits);
 
-    const CORRECT_SNOWFLAKE: i64 = 354660140149040033;
-    try std.testing.expectEqual(CORRECT_SNOWFLAKE, snowflake_int);
-
-    const data = AnimeSnowflake.fromInt(snowflake_int);
-    try std.testing.expectEqual(timestamp, data.timestamp);
-    try std.testing.expectEqual(random_bits, data.random_bits);
+    const bitwised = snowflake_int & std.math.maxInt(u63) >> 13;
+    std.debug.assert(timestamp == bitwised);
 }
 
 test "snowflake max" {
     const timestamp: i64 = std.math.maxInt(u50);
     const random_bits: u13 = std.math.maxInt(u13);
-    const snowflake = AnimeSnowflake{
+
+    const snowflake = AnimeSnowflake{ .fields = .{
         .timestamp = @intCast(u50, timestamp),
         .random_bits = random_bits,
-    };
+    } };
 
-    const snowflake_int = snowflake.toInt();
+    const snowflake_int = snowflake.value;
 
-    const data = AnimeSnowflake.fromInt(snowflake_int);
+    const data = (AnimeSnowflake{ .value = snowflake_int }).fields;
     try std.testing.expectEqual(timestamp, data.timestamp);
     try std.testing.expectEqual(random_bits, data.random_bits);
 }
