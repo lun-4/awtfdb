1511a4d1f9fc9c6c5e7fa8dac3c89169e550d07a
Luna <git@l4.pm>
2022-11-26 18:37:24 -0300

    add draft for snowflake migration

--- src/test_migrations.zig
+++ src/test_migrations.zig
@@ -59,8 +59,6 @@ fn loadSingleMigration(ctx: *Context, comptime index: usize) !void {
     const current_version: i32 = (try ctx.db.?.one(i32, "select max(version) from migration_logs", .{}, .{})) orelse 0;
     logger.info("db version: {d}", .{current_version});
 
-    // before running migrations, copy the database over
-
     var savepoint = try ctx.db.?.savepoint("migrations");
     errdefer savepoint.rollback();
     defer savepoint.commit();
@@ -72,13 +70,16 @@ fn loadSingleMigration(ctx: *Context, comptime index: usize) !void {
 
         if (index == migration.version) {
             run = true;
-
-            logger.info("running migration {d} '{s}'", .{ migration.version, migration.name });
-            var diags = sqlite.Diagnostics{};
-            ctx.db.?.execMulti(migration.sql.?, .{ .diags = &diags }) catch |err| {
-                logger.err("unable to prepare statement, got error {s}. diagnostics: {s}", .{ @errorName(err), diags });
-                return err;
-            };
+            if (migration.sql) |migration_sql| {
+                logger.info("running migration {d} '{s}'", .{ migration.version, migration.name });
+                var diags = sqlite.Diagnostics{};
+                ctx.db.?.execMulti(migration_sql, .{ .diags = &diags }) catch |err| {
+                    logger.err("unable to prepare statement, got error {s}. diagnostics: {s}", .{ @errorName(err), diags });
+                    return err;
+                };
+            } else {
+                try migration.options.function.?(ctx);
+            }
 
             try ctx.db.?.exec(
                 "INSERT INTO migration_logs (version, applied_at, description) values (?, ?, ?);",
@@ -112,6 +113,63 @@ test "validate migration 2 works" {
         \\ insert into files (file_hash, local_path) values (1, '/test.file');
     , .{});
     try loadSingleMigration(&ctx, 2);
+    const file_hash = try ctx.db.?.one(i64, "select file_hash from files where local_path = '/test.file'", .{}, .{});
+    try std.testing.expectEqual(@as(?i64, 1), file_hash);
+}
+
+fn loadMigrationUpTo(ctx: *Context, comptime upper_index: usize) !void {
+    try ctx.loadDatabase(.{});
+    try ctx.db.?.exec(manage_main.MIGRATION_LOG_TABLE, .{}, .{});
+
+    const current_version: i32 = (try ctx.db.?.one(i32, "select max(version) from migration_logs", .{}, .{})) orelse 0;
+    logger.info("db version: {d}", .{current_version});
+
+    var savepoint = try ctx.db.?.savepoint("migrations");
+    errdefer savepoint.rollback();
+    defer savepoint.commit();
+
+    var run: bool = false;
+
+    inline for (manage_main.MIGRATIONS) |migration_decl| {
+        const migration = Migration.fromTuple(migration_decl);
+
+        if (migration.version < upper_index) {
+            run = true;
+
+            if (migration.sql) |migration_sql| {
+                logger.info("running migration {d} '{s}'", .{ migration.version, migration.name });
+                var diags = sqlite.Diagnostics{};
+                ctx.db.?.execMulti(migration_sql, .{ .diags = &diags }) catch |err| {
+                    logger.err("unable to prepare statement, got error {s}. diagnostics: {s}", .{ @errorName(err), diags });
+                    return err;
+                };
+            } else {
+                try migration.options.function.?(ctx);
+            }
+
+            try ctx.db.?.exec(
+                "INSERT INTO migration_logs (version, applied_at, description) values (?, ?, ?);",
+                .{},
+                .{
+                    .version = migration.version,
+                    .applied_at = std.time.timestamp(),
+                    .description = migration.name,
+                },
+            );
+        }
+    }
+}
+
+test "validate snowflake migration works" {
+    var ctx = try makeTestContext();
+    defer ctx.deinit();
+
+    try loadMigrationUpTo(&ctx, 7);
+    try ctx.db.?.execMulti(
+        \\ insert into hashes (id, hash_data) values (1, X'7cecc98d9dc7503dcdad71adbbdf45d06667fd38c386f5d37489ea2c24d7a4dc');
+        \\ insert into files (file_hash, local_path) values (1, '/test.file');
+    , .{});
+    try loadSingleMigration(&ctx, 8);
     const file_hash = try ctx.db.?.one(i64, "select file_hash from files where local_path = '/test.file'", .{}, .{});
     try std.testing.expectEqual(@as(?i64, 1), file_hash);
 }
