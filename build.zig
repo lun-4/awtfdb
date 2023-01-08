7c5482ba8f1c3077435051cfe22b40425fb70fc5
Luna <git@l4.pm>
2023-01-08 18:32:31 -0300

    build: single executable for Debug, multiple for Release
    close #43

--- build.zig
+++ build.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
 const deps = @import("./deps.zig");
 
+const EXECUTABLES = .{
+    .{ "awtfdb-manage", "src/main.zig" },
+    .{ "awtfdb-watcher", "src/rename_watcher_main.zig" },
+    .{ "awtfdb-janitor", "src/janitor_main.zig" },
+    .{ "ainclude", "src/include_main.zig" },
+    .{ "afind", "src/find_main.zig" },
+    .{ "als", "src/ls_main.zig" },
+    .{ "arm", "src/rm_main.zig" },
+    .{ "atags", "src/tags_main.zig" },
+    .{ "awtfdb-metrics", "src/metrics_main.zig" },
+};
+
 pub fn build(b: *std.build.Builder) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
@@ -20,25 +32,38 @@ pub fn build(b: *std.build.Builder) !void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
 
-    var single_exe = b.addExecutable("wrapper-awtfdb", "src/wrapmain.zig");
-    single_exe.setTarget(target);
-    single_exe.setBuildMode(mode);
-    deps.addAllTo(single_exe);
+    if (mode == .Debug) {
+        var single_exe = b.addExecutable("wrapper-awtfdb", "src/wrapmain.zig");
+        single_exe.setTarget(target);
+        single_exe.setBuildMode(mode);
+        deps.addAllTo(single_exe);
 
-    const hardlink_install = try b.allocator.create(CustomHardLinkStep);
-    hardlink_install.* = .{
-        .builder = b,
-        .step = std.build.Step.init(
-            .custom,
-            "link the utils",
-            b.allocator,
-            CustomHardLinkStep.make,
-        ),
-        .exe = single_exe,
-    };
-    hardlink_install.step.dependOn(&single_exe.step);
+        const hardlink_install = try b.allocator.create(CustomHardLinkStep);
+        hardlink_install.* = .{
+            .builder = b,
+            .step = std.build.Step.init(
+                .custom,
+                "link the utils",
+                b.allocator,
+                CustomHardLinkStep.make,
+            ),
+            .exe = single_exe,
+        };
+        hardlink_install.step.dependOn(&single_exe.step);
 
-    b.getInstallStep().dependOn(&hardlink_install.step);
+        b.getInstallStep().dependOn(&hardlink_install.step);
+    } else {
+        // release modes build all exes separately
+        inline for (EXECUTABLES) |exec_decl| {
+            const exec_name = exec_decl.@"0";
+            const exec_entrypoint = exec_decl.@"1";
+            var tool_exe = b.addExecutable(exec_name, exec_entrypoint);
+            tool_exe.setTarget(target);
+            tool_exe.setBuildMode(mode);
+            tool_exe.install();
+            deps.addAllTo(tool_exe);
+        }
+    }
 }
 
 const CustomHardLinkStep = struct {
@@ -51,20 +76,10 @@ const CustomHardLinkStep = struct {
     fn make(step: *std.build.Step) !void {
         const self: *Self = @fieldParentPtr(Self, "step", step);
         const builder = self.builder;
-        const EXECS = .{
-            "awtfdb-manage",
-            "awtfdb-watcher",
-            "awtfdb-janitor",
-            "ainclude",
-            "afind",
-            "als",
-            "arm",
-            "atags",
-            "awtfdb-metrics",
-        };
 
-        inline for (EXECS) |exec| {
-            const full_dest_path = builder.getInstallPath(.{ .bin = {} }, exec);
+        inline for (EXECUTABLES) |exec_decl| {
+            const exec_name = exec_decl.@"0";
+            const full_dest_path = builder.getInstallPath(.{ .bin = {} }, exec_name);
             try builder.updateFile(self.exe.output_path_source.path.?, full_dest_path);
         }
     }
