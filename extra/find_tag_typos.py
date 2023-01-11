c08ca35419ed1b3a2265af72c9d84ee030706baf
Luna <git@l4.pm>
2023-01-11 00:17:54 -0300

    find_tag_typos: enable filtering by tag usage

--- extra/find_tag_typos.py
+++ extra/find_tag_typos.py
@@ -4,7 +4,10 @@
 
 import sys
 import sqlite3
+import logging
 from pathlib import Path
+
+log = logging.getLogger(__name__)
 
 try:
     from Levenshtein import distance
@@ -16,29 +19,59 @@ def main():
     path = Path.home() / "awtf.db"
     db = sqlite3.connect(f"file:{str(path)}?mode=ro", uri=True)
     db.row_factory = sqlite3.Row
+    if len(sys.argv) >= 2:
+        post_threshold = int(sys.argv[1])
+    else:
+        post_threshold = 0
 
     cur = db.cursor()
 
     res = cur.execute(
         """
-        select tag_text
+        select tag_text, core_hash
         from tag_names
         """
     )
     tags = []
     for row in res:
-        tags.append(row[0])
+        tags.append((row[0], row[1]))
 
     print(len(tags), "tags")
 
-    for tag in tags:
+    for tag, core_hash in tags:
         scores = []
+
+        metrics_count_res = cur.execute(
+            """
+            select relationship_count
+            from metrics_tag_usage_values
+            where core_hash = ?
+            order by timestamp desc
+            limit 1
+            """,
+            (core_hash,),
+        )
+        entry_row = metrics_count_res.fetchone()
+        if not entry_row:
+            log.info("tag %s has no metrics, calculating manually...", core_hash)
+
+            full_count_res = cur.execute(
+                "select count(core_hash) from tag_files where core_hash = ?",
+                (core_hash,),
+            )
+
+            usages = full_count_res.fetchone()[0]
+        else:
+            usages = entry_row[0]
+
+        if usages < post_threshold:
+            continue
 
         scope = None
         if ":" in tag:
             scope, *_ = tag.split(":")
 
-        for other_tag in tags:
+        for other_tag, _ in tags:
             if other_tag == tag:
                 continue
 
