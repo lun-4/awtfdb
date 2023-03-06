912faceb509e032581c52ac4263d5cd7ca77947b
Luna <git@l4.pm>
2023-03-06 02:47:24 -0300

    ci: attempt to add more smoktests

--- ci/smoketest.sh
+++ ci/smoketest.sh
@@ -4,3 +4,7 @@ set -eux
 zig build
 
 ./zig-out/bin/awtfdb-manage create
+echo "among us" > among.txt
+./zig-out/bin/ainclude -t amongi among.txt
+afind_output=$(./zig-out/bin/afind amongi)
+echo "$afind_output" | grep 'among.txt' -
