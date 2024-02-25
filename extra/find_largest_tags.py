d74093f37f49bf5b4d1987bbed9aaf43e2db7092
Luna <git@l4.pm>
2024-02-25 16:49:54 -0300

    add extra/find_largest_tags.py

---
+++ extra/find_largest_tags.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+
+import time
+import sys
+import sqlite3
+from pathlib import Path
+from typing import Dict, List, Set
+
+
+def main():
+    try:
+        limit = int(sys.argv[1])
+    except IndexError:
+        limit = 50
+
+    print("limit", limit, file=sys.stderr)
+
+    path = Path.home() / "awtf.db"
+    db = sqlite3.connect(f"file:{str(path)}?mode=ro", uri=True)
+    db.row_factory = sqlite3.Row
+
+    cur = db.cursor()
+
+    res = cur.execute(
+        """
+        select file_hash, local_path
+        from files
+        """
+    )
+    filemap: Dict[str, List[str]] = {}
+    tagmap: Dict[str, Set[str]] = {}
+
+    stat_map: Dict[str, int] = {}
+    start_ts = time.monotonic()
+    for row in res:
+        file_hash, local_path = row["file_hash"], row["local_path"]
+        if local_path not in stat_map:
+            stat_map[local_path] = Path(local_path).stat().st_size
+        # fetch tags
+        tcur = db.cursor()
+        tcur.execute(
+            """
+            select hashes.id as core_hash
+            from tag_files
+            join hashes
+            	on tag_files.core_hash = hashes.id
+            where tag_files.file_hash = ?
+            """,
+            (file_hash,),
+        )
+
+        for row in tcur:
+            if row["core_hash"] not in tagmap:
+                tagmap[row["core_hash"]] = {local_path}
+            else:
+                tagmap[row["core_hash"]].add(local_path)
+    end_ts = time.monotonic()
+    time_taken = round(end_ts - start_ts, 2)
+    print("took", time_taken, "seconds to fetch all files and stats", file=sys.stderr)
+
+    def _sorter(core_hash: str) -> int:
+        return sum(stat_map[p] for p in tagmap[core_hash])
+
+    sorted_tag_keys = sorted(tagmap.keys(), key=_sorter, reverse=True)
+    for core_hash in sorted_tag_keys[:limit]:
+        ccur = db.cursor()
+        ccur.execute("select tag_text from tag_names where core_hash = ?", (core_hash,))
+        row = ccur.fetchone()
+        tag_text = row["tag_text"]
+        used_bytes = _sorter(core_hash)
+        as_kb = used_bytes / 1024
+        as_mb = as_kb / 1024
+        as_gb = as_mb / 1024
+        print(f"{tag_text}\t{as_gb}\tgb")
+
+    ender_ts = time.monotonic()
+    time_taken = round(ender_ts - end_ts, 2)
+    print("took", time_taken, "seconds to sort and shit it all", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
